0f1d00a00b3a6d77db9bbddd6e052fba37aa769a
MaksimKarnaukh <Maksim.Karnaukh@student.uantwerpen.be>
2024-05-15 00:21:10 +0200

    cleaned up model class, blurred done up to scene classif.

--- src/models.py
+++ src/models.py
@@ -12,6 +12,9 @@ class EfficientNetModel:
     model trainable.
     """
     def __init__(self, num_classes=15):
+        """
+        Initialize the EfficientNetModel with the EfficientNet_B0_Weights.IMAGENET1K_V1 weights.
+        """
         super(EfficientNetModel, self).__init__()
         self.model = efficientnet_b0(weights=EfficientNet_B0_Weights.IMAGENET1K_V1)
 
@@ -22,6 +25,30 @@ class EfficientNetModel:
         # make all layers in feature extraction and classifier parts of the model trainable
         for param in self.model.parameters():
             param.requires_grad = True
+
+    def replace_classifier(self, num_classes) -> None:
+        """
+        Replace the classifier of the model with a new classifier with num_classes output units.
+        @param num_classes: int, number of output units in the new classifier
+        @return: None
+        """
+        self.model.classifier = torch.nn.Sequential(nn.Dropout(p=0.2, inplace=True),
+                                                    nn.Linear(in_features=1280, out_features=num_classes))
+
+    def set_trainable(self, freeze_feature_extraction=False) -> None:
+        """
+        Set the feature extraction layers of the model to be trainable or frozen.
+        @param freeze_feature_extraction: bool, if True, freeze the feature extraction layers of the model
+        @return: None
+        """
+        if freeze_feature_extraction:
+            for param in self.model.parameters():
+                param.requires_grad = False
+            for param in self.model.classifier.parameters():
+                param.requires_grad = True
+        else:
+            for param in self.model.parameters():
+                param.requires_grad = True
 
 
 if __name__ == "__main__":
